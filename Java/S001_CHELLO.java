d035ca9f600908063243da7d4f2501941ef452be
xNamik4ze <iceminato31@gmail.com>
2025-10-27 18:33:51 +0700

    Hello World

---
+++ Java/S001_CHELLO.java
@@ -0,0 +1,10 @@
+/*
+Viết chương trình in ra màn hình dòng chữ:
+Hello PTIT.
+ */
+
+public class S001_CHELLO {
+    public static void main (String[] args)  {
+        System.out.println("Hello PTIT.");
+    }
+}
