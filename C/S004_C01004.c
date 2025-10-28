27f20ba2ea9fbecb060b6e29fba860fd3f5840be
xNamik4ze <iceminato31@gmail.com>
2025-10-28 20:05:38 +0700

    Gia tri thap phan

---
+++ C/S004_C01004.c
@@ -0,0 +1,16 @@
+/*
+Cho số nguyên dương N không quá 9 chữ số. Hãy in ra giá trị thập phân 1/N.
+*/
+
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d", &t);
+    while (t-- > 0) {
+        int n;
+        scanf("%d", &n);
+        printf("%.15f\n", 1.0 / n);
+    }
+    return 0;
+}
