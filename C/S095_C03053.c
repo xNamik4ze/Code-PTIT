7a8a6c8c68cacfa50ffe0edf48dbfc6c4f5f9c48
xNamik4ze <iceminato31@gmail.com>
2025-10-28 21:05:43 +0700

    t

---
+++ C/S095_C03053.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool isPrime(int n) {
+    if (n < 2) return false;
+    if (n == 2) return true;
+    if (n % 2 == 0) return false;
+    for (int i = 3; i * i <= n; i += 2) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
+int main() {
+    int t;
+    scanf("%d", &t);
+    while (t-- > 0) {
+        int n;
+        scanf("%d", &n);
+        int temp = 2;
+        if (isPrime(n - 2)) printf("%d %d ", temp, n - 2);
+        for (int i = 3; i <= n / 2; i += 2) {
+            if (isPrime(i)) {
+                if (isPrime(n - i)) printf("%d %d ", i, n - i);
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
