8e05da58e44209462031f513292b12eb0c33de23
xNamik4ze <iceminato31@gmail.com>
2025-10-28 20:05:57 +0700

    Tinh tong

---
+++ C/S005_C01005.c
@@ -0,0 +1,13 @@
+/*
+Cho hai số nguyên (có thể âm) có giá trị tuyệt đối không quá 106.
+Viết chương trình tính tổng hai số 
+*/
+
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d", a + b);
+    return 0;
+}
