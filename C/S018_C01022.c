425e7a43c281302730a058b41215f4158eb9b487
xNamik4ze <iceminato31@gmail.com>
2025-10-28 20:10:07 +0700

    Tong chu so - 2

---
+++ C/S018_C01022.c
@@ -0,0 +1,22 @@
+/*
+Hãy viết chương trình tính tổng các chữ số của một số nguyên bất kỳ.
+*/
+
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d", &t);
+    while (t-- > 0) {
+        long long n;
+        scanf("%lld", &n);
+        int sum = 0;
+        while (n > 0) {
+            int temp = n % 10;
+            sum += temp;
+            n /= 10;
+        }
+        printf("%d\n", sum);
+    }
+    return 0;
+}
