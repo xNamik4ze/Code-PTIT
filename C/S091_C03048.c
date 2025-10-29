f7fef9c9ca0e25acd5de5454d235fba6c20b2298
xNamik4ze <iceminato31@gmail.com>
2025-10-29 19:05:07 +0700

    t

---
+++ C/S091_C03048.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d", &t);
+    while (t-- > 0) {
+        long long n;
+        scanf("%lld", &n);
+        if (n % 2 == 1) {
+            printf("NO\n");
+            continue;
+        }
+
+        int odd = 0, even = 0;
+        while (n > 0) {
+            int temp = n % 10;
+            if (temp % 2 == 1) odd ++;
+            else even++;
+            n /= 10;
+        }
+
+        if (odd < even) printf("YES\n");
+        else printf("NO\n");
+    }
+    return 0;
+}
