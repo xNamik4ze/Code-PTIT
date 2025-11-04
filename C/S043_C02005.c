0b52b0f007924b8a452f17e6df93dbe7f464f02b
xNamik4ze <iceminato31@gmail.com>
2025-11-04 21:27:33 +0700

    t

---
+++ C/S043_C02005.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int height, width;
+    scanf("%d %d", &height, &width);
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < i; j++) printf("~");
+        for (int j = 0; j < width; j++) printf("*");
+        printf("\n");
+    }
+    return 0;
+}
