94579399b34b9b024bff3cc2fba30c382a57da7d
xNamik4ze <iceminato31@gmail.com>
2025-11-08 04:05:46 +0700

    t

---
+++ C/S158_C02014.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int size = n * 2 - 1;
+    int center = n - 1;
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            int temp = 1 + (abs(i - center) > abs(j - center) ? abs(i - center) : abs(j - center));
+            printf("%d", temp);
+        }
+        printf("\n");
+    }
+    return 0;
+}
