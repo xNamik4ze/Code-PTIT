0faf7fd6f1633b0c48c80842e8d73ddf4287d0a5
xNamik4ze <iceminato31@gmail.com>
2025-11-09 21:15:10 +0700

    t

---
+++ C/S063_C03006.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d", &t);
+    for (int i = 1; i <= t; i++) {
+        int n, divide = 2;
+        scanf("%d", &n);
+        printf("Test %d: ", i);
+        while (divide * divide <= n) {
+            int count = 0;
+            while (n % divide == 0) {
+                count++;
+                n /= divide;
+            }
+            if (count > 0) printf("%d(%d) ", divide, count);
+            divide++;
+        }
+        if (n > 1) printf("%d(1)", n);
+        printf("\n");
+    }
+    return 0;
+}
