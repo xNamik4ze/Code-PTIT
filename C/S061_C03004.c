0c88b86a101d0c43f07f59a96c410dfa2c05c6f4
xNamik4ze <iceminato31@gmail.com>
2025-11-09 21:15:46 +0700

    t

---
+++ C/S061_C03004.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int gcdFind(int a, int b) {
+    if (b == 0) return a;
+    return gcdFind(b, a % b);
+}
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    int gcd = gcdFind(a, b);
+    long long lcm = (long long)a * b / gcd;
+    printf("%d\n%lld", gcd, lcm);
+    return 0;
+}
