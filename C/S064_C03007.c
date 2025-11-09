cdb35e8c1463b58a1ceb338d10a2c3f1a3266d57
xNamik4ze <iceminato31@gmail.com>
2025-11-09 21:15:00 +0700

    t

---
+++ C/S064_C03007.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdbool.h>
+#define MAX 1000000
+
+bool prime[MAX + 1];
+
+void sieve() {
+    for (int i = 2; i <= MAX; i++)
+        prime[i] = true;
+    for (int i = 2; i * i <= MAX; i++)
+        if (prime[i])
+            for (int j = i * i; j <= MAX; j += i)
+                prime[j] = false;
+}
+
+bool isPalindrome(int n) {
+    int r = 0, t = n;
+    while (n > 0) {
+        r = r * 10 + n % 10;
+        n /= 10;
+    }
+    return r == t;
+}
+
+int main() {
+    int t;
+    scanf("%d", &t);
+    sieve();
+
+    while (t--) {
+        int a, b, cnt = 0;
+        scanf("%d %d", &a, &b);
+
+        for (int i = a; i <= b; i++) {
+            if (prime[i] && isPalindrome(i)) {
+                printf("%d ", i);
+                cnt++;
+                if (cnt % 10 == 0) printf("\n");
+            }
+        }
+
+        if (cnt % 10 != 0) printf("\n");
+        if (t) printf("\n");
+    }
+
+    return 0;
+}
