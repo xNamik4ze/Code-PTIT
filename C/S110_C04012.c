2abd01ad4f86e1f3fedeb14fcd5e13fb7892647c
xNamik4ze <iceminato31@gmail.com>
2025-11-13 16:52:44 +0700

    t

---
+++ C/S110_C04012.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    int n;
+    int a[100], unique[100];
+    int count = 0;
+
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    for (int i = 0; i < n; i++) {
+        int freq = 0;
+        for (int j = 0; j < n; j++) {
+            if (a[j] == a[i]) freq++;
+        }
+        if (freq > 1) {
+            bool valid = true;
+            for (int j = 0; j < count; j++) {
+                if (a[i] == unique[j]) {
+                    valid = false;
+                    break;
+                } 
+            }
+            if (valid) unique[count++] = a[i];
+        }
+    }
+
+    for (int i = 0; i < count; i++) {
+        printf("%d ", unique[i]);
+    }
+    printf("\n");
+    return 0;
+}
