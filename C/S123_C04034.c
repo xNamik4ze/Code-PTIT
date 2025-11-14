2e080e9062b6a9349f84d5ddc3758e6a5ad6e94b
xNamik4ze <iceminato31@gmail.com>
2025-11-14 21:14:50 +0700

    t

---
+++ C/S123_C04034.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d", &t);
+    while(t--) {
+        int n;
+        scanf("%d", &n);
+        int a[1003], res[1003];
+        for (int i = 0; i < n; i++) scanf("%d", &a[i]);
+
+        int max = 0, count = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            if (a[i] > max) {
+                res[count++] = a[i];
+                max = a[i];
+            }
+        }
+
+        for (int i = count - 1; i >= 0; i--) {
+            printf("%d", res[i]);
+            if (i > 0) printf(" ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
