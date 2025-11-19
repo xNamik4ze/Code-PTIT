245bf38979896164a991c7c151f1cd24332e3280
xNamik4ze <iceminato31@gmail.com>
2025-11-20 02:37:53 +0700

    Liet ke cac cap so nguyen to cung nhau

--- C/S062_C03005.c
+++ C/S062_C03005.c
@@ -1,3 +1,9 @@
+/*
+Viết chương trình nhập hai số nguyên dương a,b thỏa mãn 2<a<b<100.
+Một cặp số (i,j) được gọi là nguyên tố cùng nhau nếu i ≠ j và ước số chung lớn nhất của i với j là 1
+Hãy liệt kê các cặp số nguyên tố cùng nhau trong đoạn [a,b] theo thứ tự từ nhỏ đến lớn.
+*/
+
 #include <stdio.h>
 
 int gcdFind(int a, int b) {
