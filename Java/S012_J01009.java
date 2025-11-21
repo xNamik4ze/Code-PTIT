6a79252ca5a642c30eaf6fb7424b047a676ac78f
xNamik4ze <iceminato31@gmail.com>
2025-11-21 17:09:19 +0700

    Tong giai thua

---
+++ Java/S012_J01009.java
@@ -0,0 +1,20 @@
+/*
+Viết chương trình tính tổng S = 1 + 1.2 + 1.2.3 + ... + 1.2.3...n
+ */
+
+import java.util.Scanner;
+
+public class S012_J01009 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        long product = 1;
+        long sum = 0;
+        for (int i = 1; i <= n; i++) {
+            product *= i;
+            sum += product;
+        }
+        System.out.print(sum);
+        sc.close();
+    }
+}
