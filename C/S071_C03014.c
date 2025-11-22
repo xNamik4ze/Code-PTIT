6f1cafa06fdee193397f3dfb6c63789ab8370d88
xNamik4ze <iceminato31@gmail.com>
2025-11-22 19:13:47 +0700

    Boi so chung - Uoc so chung

--- C/S071_C03014.c
+++ C/S071_C03014.c
@@ -1,6 +1,10 @@
+/*
+Tìm bội số chung nhỏ nhất và ước số chung lớn nhất của hai số nguyên dương a, b.
+*/
+
 #include <stdio.h>
 
-int gcdFind(int a, int b) {
+long long gcdFind(long long a, long long b) {
     if (b == 0) return a;
     return gcdFind(b, a % b);
 }
@@ -9,11 +13,11 @@ int main() {
     int t;
     scanf("%d", &t);
     while (t-- > 0) {
-        int a, b;
-        scanf("%d %d", &a, &b);
-        int gcd = gcdFind(a, b);
-        int lcm = a * b / gcd;
-        printf("%d %d\n", lcm, gcd);
+        long long a, b;
+        scanf("%lld %lld", &a, &b);
+        long long gcd = gcdFind(a, b);
+        long long lcm = a * b / gcd;
+        printf("%lld %lld\n", lcm, gcd);
     }
     return 0;
 }
