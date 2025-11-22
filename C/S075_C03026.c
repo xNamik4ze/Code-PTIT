5a02735479c8bf0e6a25e9cc3a839e1a1de9fafc
xNamik4ze <iceminato31@gmail.com>
2025-11-22 19:16:39 +0700

    Thuan nghich mot nua

--- C/S075_C03026.c
+++ C/S075_C03026.c
@@ -1,3 +1,10 @@
+/*
+Cho hai số nguyên dương a và b không quá 10^6.
+Hãy kiểm tra tính chất thuận nghịch của a và b. 
+Biết rằng một số được coi là thuận nghịch nếu viết ngược lại vẫn được giá trị như ban đầu (ví dụ: 8, 34543, 11233211).
+Nếu một trong hai số là thuận nghịch, số còn lại không phải thuận nghịch thì in ra YES, ngược lại in ra NO.
+*/
+
 #include <stdio.h>
 #include <stdbool.h>
 
