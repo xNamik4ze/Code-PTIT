f1bad1ce219fe05adce729689c939199bd332e5e
xNamik4ze <iceminato31@gmail.com>
2025-12-27 07:15:49 +0700

    So dao nguyen to cung nhau

--- C/S083_C03035.c
+++ C/S083_C03035.c
@@ -1,3 +1,9 @@
+/*
+Trong toán học, cặp số (a,b) được gọi là nguyên tố cùng nhau nếu ước số chung lớn nhất của a và b bằng 1.
+Cho số nguyên dương N không quá 9 chữ số.
+Hãy kiểm tra xem N và số đảo của N có phải là một cặp số nguyên tố cùng nhau hay không.
+*/
+
 #include <stdio.h>
 
 int gcd(int a, int b) {
