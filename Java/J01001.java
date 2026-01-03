3a485ded4d35d1a55f021bb37de5c421d8ed80a5
xNamik4ze <iceminato31@gmail.com>
2026-01-03 15:22:47 +0700

    Hinh chu nhat

--- Java/J01001.java
+++ Java/J01001.java
@@ -5,7 +5,7 @@ Viết chương trình tính chu vi và diện tích của hình chữ nhật đ
 
 import java.util.Scanner;
 
-public class S004_J01001 {
+public class J01001 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
