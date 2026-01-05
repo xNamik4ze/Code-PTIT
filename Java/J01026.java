0b2753c8db259e1ff45f36ad631f184ee8334509
xNamik4ze <iceminato31@gmail.com>
2026-01-05 21:39:18 +0700

    So chinh phuong

--- Java/J01026.java
+++ Java/J01026.java
@@ -5,7 +5,7 @@ Dòng đầu của dữ liệu vào ghi số bộ test, mỗi bộ test là mộ
 
 import java.util.Scanner;
 
-public class S025_J01026 {
+public class J01026 {
     public static void main (String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
