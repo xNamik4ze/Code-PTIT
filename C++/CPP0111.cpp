9ccfdc579e9e753667669755411d9953b6bb2a42
xNamik4ze <iceminato31@gmail.com>
2026-04-15 01:38:10 +0700

    So lien ke

--- C++/CPP0111.cpp
+++ C++/CPP0111.cpp
@@ -3,5 +3,23 @@
 using namespace std;
 
 void solve() {
-    
+    string s;
+    cin >> s;
+    for (int i = 0; i < s.length() - 1; i++) {
+        int dif = (int)s[i] - (int)s[i+1];
+        if (dif != -1 && dif != 1) {
+            cout << "NO" << endl;
+            return;
+        }
+    }
+    cout << "YES" << endl;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
 }
