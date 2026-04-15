d1dc79f4e5c3310786df036d110b830f5a5c1655
xNamik4ze <iceminato31@gmail.com>
2026-04-15 07:52:10 +0700

    Cap so nguyen to dau tien co tong bang n

---
+++ C++/CPP0127.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+const int MAX = 1000000;
+
+bool isPrime[MAX + 1];
+
+void sieve() {
+    fill(isPrime, isPrime + MAX + 1, true);
+    isPrime[0] = isPrime[1] = false;
+
+    for (int i = 2; i * i <= MAX; i++) {
+        if (isPrime[i]) {
+            for (int j = i * i; j <= MAX; j+= i) isPrime[j] = false;
+        }
+    }
+}
+
+void solve() {
+    int n;
+    cin >> n;
+    for (int p1 = 2; p1 <= n / 2; p1++) {
+        int p2 = n - p1;
+        if (isPrime[p1] && isPrime[p2]) {
+            cout << p1 << " " << p2 << endl;
+            return ;
+        }
+    }
+    cout << -1 << endl;
+}
+
+int main() {
+    sieve();
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
