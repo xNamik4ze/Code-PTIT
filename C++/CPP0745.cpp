d3110665da036399234cbfb82e103717bd44f33d
xNamik4ze <iceminato31@gmail.com>
2026-04-25 20:24:38 +0700

    Tinh so fibonacci lon

--- C++/CPP0745.cpp
+++ C++/CPP0745.cpp
@@ -2,22 +2,25 @@
 
 using namespace std;
 
+const long long MOD = 1000000007;
+
 void generate(long long fibo[]) {
     fibo[0] = 0;
     fibo[1] = 1;
-    for (int i = 3; i < 1001; i++) fibo[i] = (fibo[i-1] + fibo[i-2]) % MOD
+    for (int i = 2; i < 1001; i++) fibo[i] = (fibo[i-1] + fibo[i-2]) % MOD;
 }
 
 void solve(long long fibo[]) {
     int n;
     cin >> n;
-    cout << fibot[n] << endl;
+    cout << fibo[n] << endl;
 }
 
 int main() {
     long long fibo[1001];
+    generate(fibo);
     int t;
     cin >> t;
-    while (t--) solve();
+    while (t--) solve(fibo);
     return 0;
 }
